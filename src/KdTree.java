f66f04026f544644257a505fe93dbdaa3ff2bc4f
LinarAbzaltdinov <ner-1995>
2018-01-02 18:53:45 +0300

    Added Search

--- src/KdTree.java
+++ src/KdTree.java
@@ -2,6 +2,10 @@ public class KdTree {
     int dim = 2;
     Node root = null;
 
+    public KdTree(int dim) {
+        this.dim = dim;
+    }
+
     public class Node {
         int[] point;
         Node left = null, right = null;
@@ -9,6 +13,20 @@ public class KdTree {
         public Node(int[] point) {
             if (point.length == dim)
                 this.point = point;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null || !(obj instanceof Node))
+                return false;
+            Node t = (Node)obj;
+            if (t.point.length != this.point.length)
+                return false;
+            for (int i = 0; i < this.point.length; ++i) {
+                if (t.point[i] != this.point[i])
+                    return false;
+            }
+            return true;
         }
     }
 
@@ -40,6 +58,23 @@ public class KdTree {
             currDim = (currDim + 1) % dim;
         }
         return true;
+    }
+
+    public boolean search(Node t) {
+        if (dim != t.point.length)
+            return false;
+        int currDim = 0;
+        Node currNode = root;
+        while (currNode != null) {
+            if (currNode.equals(t))
+                return true;
+            if (currNode.point[currDim] <= t.point[currDim])
+                currNode = currNode.left;
+            else
+                currNode = currNode.right;
+            currDim = (currDim + 1) % dim;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
